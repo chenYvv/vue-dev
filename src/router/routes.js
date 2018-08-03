5ee43429663d253bc73f9ad157bb2638d1a4428f
chenyw <chen_yw@126.com>
2018-08-03 13:39:48 +0800

    reg login logout over

---
+++ src/router/routes.js
@@ -0,0 +1,26 @@
+export default [
+  // 登陆路由
+  {
+    path: '/auth/login',
+    name: 'Login',
+    component: () => import('@/views/auth/Login')
+  },
+  // 注册路由
+  {
+    path: '/auth/register',
+    name: 'Register',
+    component: () => import('@/views/auth/Register')
+  },
+  // 首页路由配置
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  // 其他未配置的路由都跳转到首页
+  {
+    path: '*',
+    // 重定向
+    redirect: '/'
+  }
+]
--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -84,7 +84,11 @@ export default {
           password: this.password,
           avatar: `https://api.adorable.io/avatars/200/${this.username}.png`
         }
-        const localUser = ls.getItem('user')
+        // const localUser = ls.getItem('user')
+        // 改成 从仓库获取用户信息
+        const localUser = this.$store.state.user
+
+        console.log(localUser)
 
         if (localUser) {
           if (localUser.name === user.name) {
@@ -98,7 +102,9 @@ export default {
       }
     },
     login(user) {
-      ls.setItem('user', user)
+      // ls.setItem('user', user)
+      // 改成 分发 login 事件，以保存用户信息和登录
+      this.$store.dispatch('login', user)
       this.showMsg('注册成功', 'success')
     },
     showMsg(msg, type = 'warning') {
